c98e77a1eda4b1d8e3155226957fac6800e9af26
Amitrajit <amitrajit@pop-os.localdomain>
2022-08-11 17:25:09 +0530

    init: schema initialized

--- script.ts
+++ script.ts
@@ -1,2 +1,12 @@
-import { PrismaClient } from "@prisma/client"
+// import { PrismaClient } from "@prisma/client"
+const { PrismaClient } = require("@prisma/client")
 const prisma = new PrismaClient()
+
+async function main() {
+  // const user = await prisma.user.create({ data: { name: "Leo" } })
+  // console.log(user)
+}
+
+main()
+  .catch((e) => console.log(e.message))
+  .finally(async () => await prisma.$disconnect())
